7a52bc1e971f98db4810293bfc86bfede79c0eb1
Prasoon <uprasoon@gmail.com>
2025-07-11 00:06:44 +0530

    add. PROD url

--- src/audio/services/audio.service.ts
+++ src/audio/services/audio.service.ts
@@ -2,7 +2,7 @@ import { useMutation } from '@tanstack/react-query';
 import axios from 'axios';
 
 const ENDPOINTS = {
-    PROD: "",
+    PROD: "https://web-production-8e7d.up.railway.app",
     LOCAL: "http://127.0.0.1:8000"
 } 
 
@@ -12,7 +12,7 @@ export const useEmotionPredictor = () => {
       const formData = new FormData();
       formData.append('file', file);
 
-      const res = await axios.post(`${ENDPOINTS.LOCAL}/api/predict/`, formData, {
+      const res = await axios.post(`${ENDPOINTS.PROD}/api/predict/`, formData, {
         headers: { 'Content-Type': 'multipart/form-data' },
       });
       return res.data;
